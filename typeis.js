308ecff2c48d922c72de6d90c4d1e0d4915636bc
salihsagdilek <salihsagdilek@gmail.com>
2017-10-15 03:12:22 +0300

    property usage warning

--- typeis.js
+++ typeis.js
@@ -5,25 +5,32 @@
  * Typeis. it's the smart and simple javascript type check and validation library
  *
  * @author Salih Sağdilek
- * @version 1.1.1
+ * @version 1.1.2
  */
 
 ;(function () {
-    var OP = Object.prototype;
-    if (Object.defineProperty && !OP.typeis) {
-        var toString, Regex;
-        toString = OP.toString;
-        Regex = /^\[object |]$/gi;
-        Object.defineProperty(OP, 'typeis', {value: function (is) {return typeis(this, is)}});
-        function whatTheType(something){
-            return toString.call(something).replace(Regex, '');
-        }
-        function typeis(something, is) {
-            var type = whatTheType(something, is);
-            if(whatTheType(is) == 'Array'){
-                is = is.join('|');
-            }
-            return is ? new RegExp('^(' + is + ')$', 'i').test(type) : type;
-        }
+  var OP = Object.prototype
+  if (Object.defineProperty && !OP.typeis) {
+    var toString, Regex
+    toString = OP.toString
+    Regex = /^\[object |]$/gi
+    Object.defineProperty(OP, 'typeis', {
+      value: function (is) {
+        console.warn('Property usage will not available after ^2.0 anymore, please use function typeis(variable, [type])')
+        return typeis(this, is)
+      }
+    })
+
+    function whatTheType (something) {
+      return toString.call(something).replace(Regex, '')
     }
-})();
+
+    function typeis (something, is) {
+      var type = whatTheType(something, is)
+      if (whatTheType(is) == 'Array') {
+        is = is.join('|')
+      }
+      return is ? new RegExp('^(' + is + ')$', 'i').test(type) : type
+    }
+  }
+})()
